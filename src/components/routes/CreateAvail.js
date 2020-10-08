d5c965bea0e1fa524922ce57991bca53d38863af
Ritik <ritikpr307@gmail.com>
2020-10-09 01:18:32 +0530

    added image in form

--- src/components/routes/CreateAvail.js
+++ src/components/routes/CreateAvail.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Form,Message } from 'semantic-ui-react';
+import { Form,Message,Image,Header } from 'semantic-ui-react';
+// Image
+import cat from "../../images/cat-sip.png";
 
 import "../../css/master.css";
 
@@ -26,41 +28,51 @@ class CreateAvail extends Component {
   render() {
     const { value } = this.state
     return (
-      <div className="ui segment container createAvail">
-        <Message
-          warning
-          header='Please fill all the details carefully'
-        />
-        <Form>
-          <Form.Input required fluid label='Title' placeholder='Ex.Rajma Rice'/>
-          <h4 class="ui dividing header">Food Description</h4>
-
-          {/* -------------------------------- FOOD DESCRIPTION ---------------------------- */}
-          <Form.Group widths='equal'>
-            <Form.Select required fluid label='Type' options={type} placeholder='' />
-            <Form.Select required fluid label='Category'options={category} placeholder='' />
-            <Form.Select required fluid label='State' options={state} placeholder=''/>
-          </Form.Group>
-          <Form.Group widths='equal'>
-            <Form.Input required fluid label='Date' type="date"  />
-            <Form.Input required fluid label='Quantity' type='number' placeholder='in KG'/>
-            <Form.Input required fluid label='Other' type='text' placeholder='other details'/>
-          </Form.Group>
-          <Form.Input label='Image' type='file' placeholder='other details'/>
+      <div className="createAvail">
+        <div>
+          <Header as='h2' icon textAlign='center'>
+            <Image circular src={cat} />
+            <Header.Content>Food Info.</Header.Content>
+          </Header>
+        </div>
+      
+        <div className="ui segment container ">
           
-          <h4 class="ui dividing header">Contact Details</h4>
-          <Form.Group widths='equal'>
-            <Form.Input required fluid label='Phone No.' type="text"  />
-            <Form.Input required fluid label='Email' type='email' placeholder='something with @'/>
-          </Form.Group>
-          <Form.Group widths='equal'>
-            <Form.Input required fluid label='City' type="text" placeholder="Ex.Delhi" />
-            <Form.Input required fluid label='PIN Code' type='text' placeholder='Ex.110031'/>
-          </Form.Group>
-          <Form.TextArea required label='Address' placeholder='Enter full address' value="" />
-          <Form.Checkbox required label='I agree to the Terms and Conditions' />
-          <Form.Button>Submit</Form.Button>
-        </Form>
+          <Message
+            warning
+            header='Please fill all the details carefully'
+          />
+          <Form>
+            <Form.Input required fluid label='Title' placeholder='Ex.Rajma Rice'/>
+            <h4 class="ui dividing header">Food Description</h4>
+
+            {/* -------------------------------- FOOD DESCRIPTION ---------------------------- */}
+            <Form.Group widths='equal'>
+              <Form.Select required fluid label='Type' options={type} placeholder='' />
+              <Form.Select required fluid label='Category'options={category} placeholder='' />
+              <Form.Select required fluid label='State' options={state} placeholder=''/>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Input required fluid label='Date' type="date"  />
+              <Form.Input required fluid label='Quantity' type='number' placeholder='in KG'/>
+              <Form.Input required fluid label='Other' type='text' placeholder='other details'/>
+            </Form.Group>
+            <Form.Input label='Image' type='file' placeholder='other details'/>
+            
+            <h4 class="ui dividing header">Contact Details</h4>
+            <Form.Group widths='equal'>
+              <Form.Input required fluid label='Phone No.' type="text"  />
+              <Form.Input required fluid label='Email' type='email' placeholder='something with @'/>
+            </Form.Group>
+            <Form.Group widths='equal'>
+              <Form.Input required fluid label='City' type="text" placeholder="Ex.Delhi" />
+              <Form.Input required fluid label='PIN Code' type='text' placeholder='Ex.110031'/>
+            </Form.Group>
+            <Form.TextArea required label='Address' placeholder='Enter full address' value="" />
+            <Form.Checkbox required label='I agree to the Terms and Conditions' />
+            <Form.Button>Submit</Form.Button>
+          </Form>
+        </div>
       </div>
     )
   }
