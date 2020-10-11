aff1c9c23084129d26aa1635682cf45c8639e92b
Ritik <ritikpr307@gmail.com>
2020-10-12 01:51:54 +0530

    remade dashboard

--- src/components/routes/Dashboard.js
+++ src/components/routes/Dashboard.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Rating } from "semantic-ui-react";
 
+
+// Semantic-ui react
+import { Item, Label, Segment, Container, Grid, Rating } from "semantic-ui-react";
 // Image
 import cardImg from "../../images/minion_card.svg";
 
@@ -11,70 +13,44 @@ import "../../css/master.css";
 import FoodInfo from "./FoodInfo";
 
 class Dashboard extends React.Component {
-  renderCardsList() {
+ 
+  render() {
+
     const cardList = this.props.cardDetails.map((cardDetail) => {
       return (
-        // --------------------------- C A R D S ---------------------------------------
-        <div className="card">
-          <div className="content">
-            <div className="ui grid stackable doubling">
-              <div className="six wide column image">
-                <img className="right floated large ui image" alt={cardDetail.title} src={cardImg}></img>
-              </div>
-              <div className="ten wide column">
-                <div className="">
-                  <h3>{cardDetail.title.toUpperCase()}</h3>
-                </div>
-                <div className="meta">{cardDetail.quantity}</div>
-                <div className="description">{cardDetail.description}</div>
-              </div>
-            </div>
-          </div>
-
-          <div className="extra ">
-            <Rating icon="star" defaultRating={3} maxRating={4} />
-            <div
-              className="ui heart rating"
-              data-rating="1"
-              data-max-rating="3"
-            ></div>
-
-            <span className="right floated " onClick={<FoodInfo/>}>
-              <Link to="/foodinfo"> 
-                {cardDetail.expired === true ? (<i className="close icon red "></i>) : (<i className="check circle icon blue"></i>)}
-              </Link>
-
-            </span>
-          </div>
-        </div>
-        //---------------------------------------------------------------------------
+        <Grid.Column>
+          <Segment className="details">
+            <Item.Group divided>
+              <Item >
+                <Item.Image size="small" src={cardImg} />
+                <Item.Content>
+                  <Item.Header >{cardDetail.title}</Item.Header>
+                  <Item.Meta>{cardDetail.quantity}</Item.Meta>
+                  <Item.Description>
+                    <p>
+                      {cardDetail.description}
+                    </p>
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+            </Item.Group>
+          </Segment>
+        </Grid.Column>
       );
     });
-    return cardList;
-  }
-  render() {
-    // console.log(this.props.cardDetails);
     return (
-      <div className="dashboard ui grid container">
-        <div className="row">
-          <div className="sixteen wide column content">
-            <div className="ui segment row facts">
-              <h1>food facts</h1>
-              <p>
-
-                shishye is sad even though senpai made so much efforts to make
-                her happy and she is also not eating..... i wonder why she is
-                like that sometimes although i was thinking about owning a
-                skeleton and now i have one....
-                <br />
-              </p>
-            </div>
-            <div className="ui cards two stackable doubling">
-              {this.renderCardsList()}
-            </div>
-          </div>
-        </div>
+      <div >
+        <Container className="dashboard ">
+          <Grid columns={2}>
+            <Grid.Row className="ui stackable doubling">
+                {cardList}
+            </Grid.Row>
+            
+          </Grid>
+          
+        </Container>
       </div>
+      
     );
   }
 }
--- src/components/routes/FoodInfo.js
+++ src/components/routes/FoodInfo.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Container,Header, Item, Segment, Input,TextArea, Form } from "semantic-ui-react";
+import { Container,Header, Item, Segment,TextArea, Form,Button,Portal,TransitionablePortal } from "semantic-ui-react";
 
 // Image
-import minion_luv from "../../images/minion_luv.svg";
+import minion_luv from "../../images/cage.jpg";
 import minion_graph from "../../images/minion_graph.svg";
 import sip from "../../images/sip.svg";
 
@@ -10,7 +10,13 @@ import sip from "../../images/sip.svg";
 import "../../css/master.css";
 
 class FoodInfo extends React.Component {
+  state = { animation: "zoom", duration: 500,open: false }
+
+  handleClose = () => this.setState({ open: false })
+  handleOpen = () => this.setState({ open: true })
+
   render = () => {
+    const { open,animation,duration } = this.state
     return (
       <Container className="foodinfo">
         <div>
@@ -88,7 +94,42 @@ class FoodInfo extends React.Component {
                 
               </Item.Content>
             </Item>
+            <Item>
+              <Item.Content>
+
+                <Button
+                  content='Open Portal'
+                  disabled={open}
+                  positive
+                  onClick={this.handleOpen}
+                />
+
+                <TransitionablePortal onClose={this.handleClose} open={open} transition={{ animation, duration }}>
+                  <Segment className="chithi"
+                    style={{
+                      left:'40vw',
+                      
+
+                      position: 'fixed',
+                      top: '50%',
+                      zIndex: 1000,
+                    }}
+                  >
+                    <Header>Your Referal Code: <em>12345</em> </Header>
+                    
+                    <Button
+                      content='Close Portal'
+                      negative
+                      onClick={this.handleClose}
+                    />
+                  </Segment>
+                </TransitionablePortal>
+
+
+              </Item.Content>
+            </Item>
           </Item.Group>
+          
         </Segment>
       </Container>
     );
