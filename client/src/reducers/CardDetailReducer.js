c405f90c9134bad0cb34cd06316a8209485a1f10
Ritik <ritikpr307@gmail.com>
2021-01-29 21:15:24 +0530

    added code for streamr

--- client/src/reducers/CardDetailReducer.js
+++ client/src/reducers/CardDetailReducer.js
@@ -2,6 +2,16 @@ import { ADD_DETAILS, FILL_DASHBOARD } from "../actions/index";
 import axios from "axios";
 
 export default function (state = [], action) {
+  //---------------------------streamr init begins--------------------------
+  const StreamrClient = require('streamr-client')
+
+  const client = new StreamrClient({
+      auth: {
+          privateKey: 'af50f762733d5a84142684a3f619d1382ceced77c0d7312b864794ad8e5b1d0a',
+      }
+  })
+  //--------------------------streamr init ends------------------------------
+
   switch (action.type) {
     case ADD_DETAILS:
       console.log("from reducer: ", action.payload.values);
@@ -17,6 +27,16 @@ export default function (state = [], action) {
           console.log("error occured on the client side");
         });
       console.log("to check ", state);
+      
+      //publishing data to streamr
+      client.publish('0x93a44e793341bc2a1d7eaf35c7313a4e7563157d/OnlyMeal',action.payload.values)
+            .then(()=>{
+              console.log("data sent successfully");
+            })
+            .catch((err)=>{
+              console.log("error while sending data to streamr: ",err);
+            });
+      
       return state;
     
     case FILL_DASHBOARD:
